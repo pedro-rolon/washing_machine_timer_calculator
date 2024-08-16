6ef057554ab1c6cdf8c8839cc89afaa66aec92d3
Pedro Rolon <pedrito.rolon94@gmail.com>
2024-08-16 16:33:58 -0400

    cambios varios

---
+++ calculator.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  document.getElementById("centralCard").onclick = function () {
+    onClick();
+  };
+
+  document.getElementById("btnCalcular").onclick = function () {
+    alert("pendiente de implementacion");
+  };
+});
+
+function onClick() {}
+
+function calcularDelay(horaObjetivo, duracion, horaActual) {
+  return number * number;
+}
+
+// calcular
+// hora objetivo
+// cuanto dura el lavado
+// hora actual
+
+// hora objetivo - duracion - hora actual
+
+// hora objetivo = duracion + delay
+
+// hora en que tiene que empezar a lavarse = hora objetivo - duracion
+
+// delay = hora objetivo - duracion - hora actual
